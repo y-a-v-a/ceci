f5bf29642ac848cddf136b22e43a2f432a003c79
Vincent Bruijn <vebruijn@gmail.com>
2013-07-05 23:21:52 +0200

    added creators info

--- index.php
+++ index.php
@@ -4,9 +4,18 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Responsive Magritte</title>
+		<title>Responsive Magritte • ax710 • y-a-v-a.org</title>
+		<meta name="description" content="Responsive Magritte - ax710 - y_a_v_a">
+		<meta name="keywords" content="ax710, Rene Magritte, y-a-v-a, Responsive pipe, responsive magritte, la trahison des images responsively">
+
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+		<link rel="shortcut icon" href="favicon.png">
 		<link rel="stylesheet" href="style.css">
+		<!--
+			la-trahison-des-images.be en ceci-n-est-pas-une-pipe.be van ax710 en _y_a_v_a_
+			is in licentie gegeven volgens een
+			Creative Commons Naamsvermelding 3.0 Nederland licentie
+		-->
 	</head>
 	<body>
 		<article>
@@ -27,6 +36,11 @@
 			<img id="signature" src="images/magritte.png" width="30" height="13">
 		</article>
 
+		<footer>
+		<a href="http://la-trahison-des-images.be">la-trahison-des-images.be</a> en <a href="http://ceci-n-est-pas-une-pipe.be/">ceci-n-est-pas-une-pipe.be</a>
+		 van <a href="http://www.ax710.org/" rel="cc:attributionURL">ax710</a> &amp; <a href="http://www.y-a-v-a.org/" rel="cc:attributionURL">y-a-v-a.org</a>
+			is in licentie gegeven volgens een <a rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">Creative Commons Naamsvermelding 3.0 Nederland licentie</a>.
+		</footer>
 		<script>
 		  var _gaq = _gaq || [];
 		  _gaq.push(['_setAccount', 'UA-xxxxx-x']);
