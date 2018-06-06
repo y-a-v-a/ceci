29a22462cd8356bd9e853a868293814c55927d15
Vincent Bruijn <vebruijn@gmail.com>
2018-06-06 10:05:42 +0200

    flex updates, adds favicon

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 		<link rel="shortcut icon" href="favicon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
 		<link rel="stylesheet" href="style.css">
 		<!--
 			la-trahison-des-images.be van ax710 en _y_a_v_a_
@@ -18,38 +20,39 @@
 		-->
 	</head>
 	<body>
-		<article>
-			<div class="image">
-				<div class="ceci">
-				    <img src="images/ceci.png" alt="" height="270" width="167">
+		<div class="wall">
+			<figure>
+				<div class="image">
+					<div class="ceci">
+						<img src="images/ceci.png" alt="" height="270" width="167">
+					</div>
+					<div class="n-est-pas">
+						<img src="images/n-est-pas.png" alt="" height="270">
+					</div>
+					<div class="une-pipe">
+						<img src="images/une-pipe.png" alt="" height="270" width="57">
+					</div>
 				</div>
-				<div class="n-est-pas">
-				    <img src="images/n-est-pas.png" alt="" height="270">
+				<div class="trahison">
+					<img src="images/ceci-n-est-pas-une-pipe.png" width="366" height="41">
 				</div>
-				<div class="une-pipe">
-				    <img src="images/une-pipe.png" alt="" height="270" width="57">
-				</div>
-			</div>
-			<div class="trahison">
-				<img src="images/text.png" width="366" height="41">
-			</div>
-			<img id="signature" src="images/magritte.png" width="30" height="13">
-		</article>
+				<img id="signature" src="images/magritte.png" width="30" height="13">
+			</figure>
+		</div>
 
 		<footer>
-		<a href="http://la-trahison-des-images.be">la-trahison-des-images.be</a> van <a href="http://www.ax710.org/" rel="cc:attributionURL">ax710</a>
+		  <a href="http://la-trahison-des-images.be">la-trahison-des-images.be</a> van <a href="http://www.ax710.org/" rel="cc:attributionURL">ax710</a>
         &amp; <a href="http://www.y-a-v-a.org/" rel="cc:attributionURL">y-a-v-a.org</a> is in licentie gegeven volgens een
         <a rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">Creative Commons Naamsvermelding 3.0 Nederland licentie</a>.
 		</footer>
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-          ga('create', 'UA-16179947-17', 'la-trahison-des-images.be');
-          ga('send', 'pageview');
-
-        </script>
+      ga('create', 'UA-16179947-17', 'la-trahison-des-images.be');
+      ga('send', 'pageview');
+    </script>
 	</body>
 </html>
